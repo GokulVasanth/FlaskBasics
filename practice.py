9501a1060c2309a2a4175d56850d500964f60064
Gokul Vasanth Susindran <gokulvasanth.s@ad.infosys.com>
2019-09-15 22:52:12 +0530

    added code for reading files and directories from a given path in prctice file

--- practice.py
+++ practice.py
@@ -1,13 +1,26 @@
-al ='Gokul@48'
-print(al[0].isupper())
-print(al[-1].isdigit())
+import os
+import pprint
+import sys
 
-uppercase = False
-lowercase = False
-while lowercase and uppercase:
-    for i in al:
-        if i.isupper():
-            uppercase = True
-        elif i.islower():
-            lowercase = True
-    
+path = input(" Please enter the directory Name: ")
+if os.path.exists:
+    list_files_dir = os.listdir(path)
+else:
+    pprint.pprint("Please enter a valid path")
+    sys.exit()
+
+# pprint.pprint(list_files_dir)
+list_files = []
+list_dir = []
+
+for files in list_files_dir:
+    p = os.path.join(path,files)
+    if os.path.isfile(p):
+        list_files.append(p)
+    else: 
+        list_dir.append(p)
+
+pprint.pprint(list_dir)
+pprint.pprint(list_files)
+
+# pprint.pprint(list_dir,list_files)
